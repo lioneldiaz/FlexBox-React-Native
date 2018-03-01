fbedd35999256779a216cf8ca7067232b0795bca
DevelopHack <lionel2688@gmail.com>
2018-03-01 12:34:22 -0600

    feat: a new feature

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, AppRegistry } from 'react-native'
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 class Flexbox extends Component {
+  constructor(){
+    super()
+    this.state = { isShow: 0 }
+    setInterval(() => {
+      this.setState( 
+       { isShow: getRandomInt(4)}
+      )
+    }, 1000)
+  }
   render() {
-    return (
-      <View style={styles.container}>
+    return (  
+      <View style={
+        this.state.isShow === 0 
+        ? styles.container1 
+        : this.state.isShow === 1 
+        ? styles.container2 
+        : this.state.isShow === 2
+        ? styles.container3
+        : styles.container4}>
         <View style={styles.box}/>
         <View style={styles.box}/>
         <View style={styles.box}/>
@@ -14,16 +34,31 @@ class Flexbox extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container1: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  container2: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  container3: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  container4: {
+    flex: 1,
+    justifyContent: 'flex-end'
   },
   box: {
     height: 50,
     width: 50,
     backgroundColor: 'green',
-    margin: 10,    
+    margin: 10,   
   }
 })
 
